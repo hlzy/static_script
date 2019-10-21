dc31537cc42fe83f65752182985b82d0ceba8a51
hlzy <hlpower89@gmail.com>
2019-10-21 15:25:49 +0800

    xx

---
+++ hd_af_preformance.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import sys
+
+file_name = sys.argv[1]
+file1 = pd.read_csv(file_name)
+af_block_value = file1[~file1["blocked_reason"].isnull()].value_counts()
+af_block_names = af_block_value.keys().tolist()
+hd_block_value = file1[~file1["anti_type"].isnull()].value_counts()
+hd_block_names = hd_block_value.keys().tolist()
+
+
+str1 = " \t \t %s\r\n" % ("\t".join(af_block_name))
+str2 = " \t \t %s\r\n" % ("\t".join(af_block_value.tolist()))
+
+with open("af_hd_result.csv","w") as f:
+    f.write(str1)
+    f.write(str2)
+    for each_hd in hd_block_names:
+        str3 = "%s\t%d" % (each_hd,hd_block_value[each_hd])
+        mlist = []
+        for each_af in af_block_names:
+            join_result = sum((file1["anti_type"] == each_hd)&(file1['anti_type'] == each_af))
+            mlist.append(join_result)
+        str3 = "%s\t%s" % (str3,"\t".join([str(i) for i in mlist]))
+    f.write(str3)
